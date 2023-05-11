4209df51b6f330872778c1a62099c6cf679004a7
Sai Charan <92617073+saicharan20012007@users.noreply.github.com>
2023-05-11 16:46:09 +0530

    login app.js

--- app.js
+++ app.js
@@ -55,54 +55,6 @@ const main = async () => {
 main();
 
 
-// Google Authentication
-
-// passport.use(new GoogleStrategy({
-//   clientID: process.env.GOOGLE_CLIENT_ID,
-//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   callbackURL: '/auth/google/callback'
-// }, (accessToken, refreshToken, profile, done) => {
-//   const user = {
-//     googleId: profile.id,
-//     email: profile.emails[0].value,
-//     name: profile.displayName
-//   };
-//   done(null, user);
-// }));
-
-// passport.serializeUser((user, done) => {
-//   done(null, user.googleId);
-// });
-
-// passport.deserializeUser((id, done) => {
-//   done(null, { googleId: id });
-// });
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// app.get('/auth/google', passport.authenticate('google', {
-//   scope: ['profile', 'email']
-// }));
-
-// app.get('/auth/google/callback', passport.authenticate('google', {
-//   failureRedirect: '/login'
-// }), (req, res) => {
-//   res.redirect('/');
-// });
-
-// app.get('/api/user', (req, res) => {
-//   if (req.user) {
-//     res.send(req.user);
-//   } else {
-//     res.status(401).send('Unauthorized');
-//   }
-// });
-
-// app.get('/logout', (req, res) => {
-//   req.logout();
-//   res.redirect('/');
-// });
 
 
 
@@ -165,81 +117,14 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.listen(4000, () => {
-  console.log("Server listening on port 3002");
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
 });
 
 // Test API
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   console.log("Test API");
-  
   res.header('Access-Control-Allow-Origin', '*');
   res.send("Test API is Working fine!");
 });
 
-// Define an API endpoint that retrieves the sum of the numbers array for a given email
-app.get("/score/:email", async (req, res) => {
-  try {
-    // Retrieve the email parameter from the request
-    const email = req.params.email;
-
-    // Find the user document with the given email
-    const person = await client
-      .db("prabisha")
-      .collection("users")
-      .findOne({ email });
-    console.log(person);
-    // If the user doesn't exist, return an error response
-    if (!person) {
-        res.header('Access-Control-Allow-Origin', '*');
-
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    // Loop through the array and sum the numbers field
-    let sum = 0;
-    person.scores.forEach((obj) => {
-      sum += obj.numbers.reduce((total, current) => total + current, 0);
-    });
-
-    // Return the sum as a response
-    res.json({ sum });
-  } catch (error) {
-    // Handle any errors that occur during the process
-    console.error(error);
-      res.header('Access-Control-Allow-Origin', '*');
-
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// Define an API endpoint that retrieves the array based on the user's email
-app.get("/certificates/:email", async (req, res) => {
-  try {
-    // Retrieve the email parameter from the request
-    const email = req.query.email;
-
-    // Find the document with the given email
-    const person = await client
-      .db("prabisha")
-      .collection("users")
-      .findOne({ email });
-    console.log(person);
-    // If the document doesn't exist, return an error response
-    if (!person) {
-      return res.status(404).json({ error: "Document not found" });
-    }
-
-    // Extract the array from the document
-    const certificates = person.certificates;
-
-    // Return the array as a response
-      res.header('Access-Control-Allow-Origin', '*');
-
-    res.json({ certificates });
-  } catch (err) {
-    // Return an error response if an error occurs
-    res.header('Access-Control-Allow-Origin', '*');
-    res.status(500).json({ error: err.message });
-  }
-});
